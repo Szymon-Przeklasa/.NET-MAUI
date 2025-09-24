5bf8a627ccd9908f2abc5653b933b4e8d0b7c698
Szymon-Przeklasa <s.przeklasa.tp22@zsme.tarnow.pl>
2025-09-24 11:24:44 +0200

    basic app layout - not finished

---
+++ TODOLIst/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TODOLIst")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TODOLIst.Pages")]
---
+++ TODOLIst/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace TODOLIst
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
